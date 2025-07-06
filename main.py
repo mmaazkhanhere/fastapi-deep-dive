6d4f9a5dee621e1c18988a6328af5ab37fde6cfd
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2025-07-06 23:18:15 +0500

    forms, files, errors, and body updates covered

--- main.py
+++ main.py
@@ -1,96 +1,104 @@
-# main.py
-from fastapi import FastAPI, Query, Path, Body
-from enum import Enum
-from typing import List, Dict, Union, Annotated
-from pydantic import BaseModel, Field, EmailStr
+from typing import Annotated
+from pydantic import BaseModel
+from fastapi import FastAPI, Form, UploadFile, File, HTTPException, Request
+from fastapi.responses import HTMLResponse, JSONResponse
+
+class FormData(BaseModel):
+    username: str
+    password: str
+
+class UnicornException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
+items = {"foo": "The Foo Wrestlers"}
 
 class Item(BaseModel):
-    model_config = {"extra": "forbid"}
-
-    id: int = Field(description="Unique identifier for the item")
-    name: str = Field(description="Name of the item")
-    description: str | None = Field(description="Description of the item", default=None)
-    price: float = Field(description="Price of the item", examples=["12.33"])
-    tax: float | None = Field(description="Tax to be paid on the item", default=None)
-
-class User(BaseModel):
-    model_config = {"extra": "forbid"}
-
-    id: int = Field(description="Unique identifier for the user")
-    name: str = Field(description="Name of the user", max_length=50)
-    email: EmailStr = Field(description="Email of the user")
-    password: str = Field(description="Password of the user")
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: set[str] = set()
 
 
-app = FastAPI()
+
+
+app: FastAPI = FastAPI()
+
+@app.post(
+    "/items/",
+    response_model=Item,
+    summary="Create an item",
+    description="Create an item with all the information, name, description, price, tax and a set of unique tags",
+)
+async def create_item(item: Item):
+    return item
+
+@app.post("/login/", response_description="The created item",)
+async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return {
+        "username": username, 
+        "password": password
+        }
+
+@app.post("/user")
+def get_user(data: Annotated[FormData, Form()]):
+    return data;
+
+
+@app.post("/files/")
+async def create_fil(file: Annotated[bytes, File()]): # 'Content-Type: multipart/form-data' \
+    return {"file_size", len(file)}
+
+@app.post("/uploadfile/")
+async def create_upload_file(
+    file: Annotated[UploadFile, File(description="A file read as UploadFile")],
+):
+    return {"filename": file.filename}
+
+@app.post("/multiple_files")
+async def upload_multiple_files(files: list[UploadFile]):
+    return {"filenames": [file.filename for file in files]}
 
 @app.get("/")
-def read_root():
-    return {"Status": "FastAPI Server Running"}
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
 
-# @app.put("/items/{item_id}")
-# def update_item(item_id: int, item: Item):
-#     return {"item_id": item_id, "item": item}
 
-@app.put("/users/{user_id}")
-def update_user(user_id: int, user: User, response_model=User):
-    return {"user_id": user_id, "user": user}
+@app.get("/items-header/{item_id}")
+async def read_item_header(item_id: str, tags=["Items"]):
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404,
+            detail="Item not found",
+            headers={"X-Error": "There goes my error"},
+        )
+    return {"item": items[item_id]}
 
-@app.put('/update')
-def update_all(item_id: int, item: Item, user: User):
-    return {
-        "item": item,
-        "user": user
-    }
 
-@app.put('/user')
-def update_user_data(user_id: int, user: User, session_id: Annotated[str, Body( 
-    examples=[
-        "adasdasdasd"
-    ]
-)]): # you are giving example of what the data will be like. It is already in the schema but here you are giving specific example
-    return {
-        "user_id": user_id,
-        "user": user,
-        "session_id": session_id
-    }
+@app.exception_handler(UnicornException)
+async def unicorn_exception_handler(request: Request, exc: UnicornException):
+    return JSONResponse(
+        status_code=418,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+    )
 
-@app.put("/items/{item_id}")
-async def update_item(
-    *,
-    item_id: int,
-    item: Annotated[
-        Item,
-        Body(
-            openapi_examples={
-                "normal": {
-                    "summary": "A normal example",
-                    "description": "A **normal** item works correctly.",
-                    "value": {
-                        "name": "Foo",
-                        "description": "A very nice Item",
-                        "price": 35.4,
-                        "tax": 3.2,
-                    },
-                },
-                "different": {
-                    "summary": "An example with converted data",
-                    "description": "FastAPI can convert price `strings` to actual `numbers` automatically",
-                    "value": {
-                        "name": "Bar",
-                        "price": "35.4",
-                    },
-                },
-                "invalid": {
-                    "summary": "Invalid data is rejected with an error",
-                    "value": {
-                        "name": "Baz",
-                        "price": "thirty five point four",
-                    },
-                },
-            },
-        ),
-    ],
-):
-    results = {"item_id": item_id, "item": item}
-    return results
+
+@app.get("/unicorns/{name}")
+async def read_unicorn(name: str):
+    """A function that raises custom error depending on the name entered""" #
+    if name == "yolo":
+        raise UnicornException(name=name)
+    return {"unicorn_name": name}
