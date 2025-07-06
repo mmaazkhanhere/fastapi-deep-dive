64782829fbf62e71a0d519a103a21ee32917c422
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2025-07-06 14:28:53 +0500

    request body covered

--- main.py
+++ main.py
@@ -2,106 +2,37 @@
 from fastapi import FastAPI
 from enum import Enum
 from typing import List, Dict, Union
+from pydantic import BaseModel, Field
+
+class Item(BaseModel):
+    id: int = Field(description="Unique identifier for the item")
+    name: str = Field(description="Name of the item")
+    description: str | None = Field(description="Description of the item", default=None)
+    price: float = Field(description="Price of the item")
+    tax: float | None = Field(description="Tax to be paid on the item", default=None)
+
 
 app = FastAPI()
-
-class ItemInventoryStatus(str, Enum):
-    in_stock = "Available"
-    out_of_stock = "Out of Stock"
-
-items = [
-    {
-        "id": 1,
-        "item": "Milk",
-        "price": 15.6,
-        "status": ItemInventoryStatus.in_stock
-    },
-    {
-        "id": 2,
-        "item": "Bread",
-        "price": 10.5,
-        "status": ItemInventoryStatus.out_of_stock
-    },
-    {
-        "id": 3,
-        "item": "Eggs",
-        "price": 20.0,
-        "status": ItemInventoryStatus.in_stock
-    },
-    {
-        "id": 4,
-        "item": "Cheese",
-        "price": 30.0,
-        "status": ItemInventoryStatus.out_of_stock
-    },
-    {
-        "id": 5,
-        "item": "Yogurt",
-        "price": 25.0,
-        "status": ItemInventoryStatus.in_stock
-    },
-    {
-        "id": 6,
-        "item": "Chicken",
-        "price": 40.0,
-        "status": ItemInventoryStatus.out_of_stock
-    },
-    {
-        "id": 7,
-        "item": "Beef",
-        "price": 50.0,
-        "status": ItemInventoryStatus.in_stock
-    },
-    {
-        "id": 8,
-        "item": "Pork",
-        "price": 35.0,
-        "status": ItemInventoryStatus.out_of_stock
-    },
-    {
-        "id": 9,
-        "item": "Fish",
-        "price": 45.0,
-        "status": ItemInventoryStatus.in_stock
-    },
-    {
-        "id": 10,
-        "item": "Shrimp",
-        "price": 55.0,
-        "status": ItemInventoryStatus.out_of_stock
-    }
-]
 
 @app.get("/")
 def read_root():
     return {"Status": "FastAPI Server Running"}
 
-@app.get("/items/{item_id}")  #sames as localhost:8000/items/1
-def read_item(item_id: int): #these are the parameters required to consume the api. They cannot have default values because are required fields
-    return {f"Item with id {item_id}: ": items[item_id-1].get("item")}
+# @app.post('/create_item')
+# def create_item(item: Item): # we say that we will receive request body of schema Item and we are returning it
+#     return item
+
+@app.post('/create_item') # http://localhost:8000/create_item
+def create_item(item: Item):
+    if item.tax is not None:
+        item.price = item.price + item.tax
+        return item
+    else:
+        return item
     
 
-@app.get("/items")
-def get_item(id: Union[int, None] = None): #same as localhost:8000/items?id=1 They can have default values because they are not required fields
-    if id is not None:
-        return {f"Item with id {id}: ": items[id-1].get("item")}
-    else:
-        return {"All items: ": items}
-    
-
-@app.get("/items/{item_id}/status") #same as localhost:8000/1/status?status=Available
-def get_item_statuses(item_id: int, status: Union[ItemInventoryStatus, None] = None ): # item_id is required field (path parameter) and status can have default value (query parameter)
-
-    if status == ItemInventoryStatus.in_stock:
-        return {"Item available"}
-    else:
-        return {"Out of stock"}
+@app.put("/items/{item_id}") # http://localhost:8000/items/1 (In practice, this is done to update an item of id {item_id} in the database with the new item details {item}.)
+def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.model_dump()}
 
 
-@app.get("/items/")
-def get_item_price(item_id: int, price: bool=False): #Here the item_id is a required query parameter while price is optional query parameter. For a query parameter to be made optional, assign a default value
-    if price:
-        item_price = items[item_id-1].get("price")
-        return {f"Item with id {item_id} price is: {item_price}"}
-    else:
-        return items[item_id-1]
